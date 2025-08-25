39a0b1e3addf362abfe248f47271ae9454f06d6f
Johannes Sochor <info@johannes-sochor.de>
2025-08-25 23:33:51 +0200

    noerd

---
+++ database/factories/CmsSettingFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Noerd\Cms\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Noerd\Cms\Models\CmsSetting;
+use Noerd\Cms\Models\Page;
+use Noerd\Noerd\Models\Tenant;
+
+/**
+ * @extends Factory<CmsSetting>
+ */
+class CmsSettingFactory extends Factory
+{
+    protected $model = CmsSetting::class;
+
+    public function definition(): array
+    {
+        $tenant = Tenant::factory()->create();
+        $page = Page::factory()->create(['tenant_id' => $tenant->id]);
+
+        return [
+            'tenant_id' => $tenant->id,
+            'homepage_page_id' => $page->id,
+        ];
+    }
+}
+
+
--- src/Models/CmsSetting.php
+++ src/Models/CmsSetting.php
@@ -4,12 +4,20 @@ namespace Noerd\Cms\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class CmsSetting extends Model
 {
+    use HasFactory;
     protected $table = 'cms_settings';
 
     protected $fillable = [
         'tenant_id',
         'homepage_page_id',
     ];
+
+    protected static function newFactory()
+    {
+        return \Noerd\Cms\Database\Factories\CmsSettingFactory::new();
+    }
 }
