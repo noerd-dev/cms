f49051451e6263481402c36c3aa5491a678f0461
Johannes Brock <info@johannes-brock,de>
2025-09-10 21:01:48 +0200

    noerd

---
+++ resources/views/livewire/test-component.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Noerd\Noerd\Traits\Noerd;
+use Livewire\Volt\Component;
+
+new class () extends Component {
+    use Noerd;
+
+    public const COMPONENT = 'test-component';
+} ?>
+
+<x-noerd::page :disableModal="$disableModal">
+    <x-slot:header>
+        <x-noerd::modal-title>{{ __('Page') }}</x-noerd::modal-title>
+    </x-slot:header>
+</x-noerd::page>
