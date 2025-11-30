66d3065f003a6cab697578496737656b77cce49b
Johannes Brock <info@johannes-brock,de>
2025-11-30 17:05:05 +0100

    default yml

--- resources/views/components/headerimage-action-bar.blade.php
+++
@@ -1,9 +0,0 @@
-<x-noerd::tabs>
-    <x-noerd::tab tabNumber=1>
-        Titelbild
-    </x-noerd::tab>
-
-    <x-noerd::tab tabNumber=2>
-        Logo
-    </x-noerd::tab>
-</x-noerd::tabs>
--- resources/views/components/page-action-bar.blade.php
+++
@@ -1,9 +0,0 @@
-<x-noerd::tabs>
-    <x-noerd::tab tabNumber=1>
-        Allgemein
-    </x-noerd::tab>
-
-    <x-noerd::tab tabNumber=2>
-        Metadaten
-    </x-noerd::tab>
-</x-noerd::tabs>
