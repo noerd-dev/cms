abd62a178aa3cd8f2fdcb8de38e3ab9767da132d
Johannes Brock <info@johannes-brock.de>
2026-01-12 22:59:40 +0100

    update

--- resources/views/livewire/page-detail.blade.php
+++ resources/views/livewire/page-detail.blade.php
@@ -541,37 +541,34 @@ new class () extends Component {
                 @if($collectionLayout)
                     @include('noerd::components.detail.block', $collectionLayout)
                 @endif
+
+                {{-- Elements für Tab 1 --}}
+                @if($this->page->id && $this->hasPageFeatures)
+
+                    <div x-sort="$wire.elementSort($item, $position)">
+                        @foreach($this->page->elements as $loopIndex => $elementPage)
+                            <div x-sort:item="{{$elementPage->id}}" wire:key="sort-item-{{$elementPage->id}}">
+                                <livewire:element-page-detail
+                                    wire:key="element-page-{{$elementPage->id}}"
+                                    :modelId="$elementPage->id"
+                                >
+                                </livewire:element-page-detail>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="mt-8 mb-8 flex">
+                        <div class="mt-4 mx-auto">
+                            <x-noerd::buttons.primary
+                                wire:click="$dispatch('noerdModal', {component: 'element-picker-modal', arguments: { token: 'insert-end' }})">
+                                {{ __('Add Element') }}
+                            </x-noerd::buttons.primary>
+                        </div>
+                    </div>
+
+                @endif
             </x-slot:tab1>
         </x-noerd::tab-content>
-
-        {{-- Elements nur für Tab 1 --}}
-        <div x-show="currentTab === 1">
-
-            @if($this->page->id && $this->hasPageFeatures)
-
-                <div x-sort="$wire.elementSort($item, $position)">
-                    @foreach($this->page->elements as $loopIndex => $elementPage)
-                        <div x-sort:item="{{$elementPage->id}}" wire:key="sort-item-{{$elementPage->id}}">
-                            <livewire:element-page-detail
-                                wire:key="element-page-{{$elementPage->id}}"
-                                :modelId="$elementPage->id"
-                            >
-                            </livewire:element-page-detail>
-                        </div>
-                    @endforeach
-                </div>
-
-                <div class="mt-8 mb-8 flex">
-                    <div class="mt-4 mx-auto">
-                        <x-noerd::buttons.primary
-                            wire:click="$dispatch('noerdModal', {component: 'element-picker-modal', arguments: { token: 'insert-end' }})">
-                            {{ __('Add Element') }}
-                        </x-noerd::buttons.primary>
-                    </div>
-                </div>
-
-            @endif
-        </div>
     </div>
 
     <x-slot:footer>
