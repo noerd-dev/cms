d5a6dd752e5046db0b83420a714c37df90890cf9
Johannes Brock <info@johannes-brock.de>
2026-03-18 23:23:36 +0100

    add migration

---
+++ database/migrations/2026_03_18_150712_add_additional_fields_to_pages_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (! Schema::hasColumn('pages', 'additional_fields')) {
+            Schema::table('pages', function (Blueprint $table) {
+                $table->json('additional_fields')->nullable()->after('data');
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropColumn('additional_fields');
+        });
+    }
+};
