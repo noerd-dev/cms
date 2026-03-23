3f1c7173668637fc684713c327bbe8510574318d
Johannes Brock <info@johannes-brock.de>
2026-03-23 21:51:17 +0100

    Add collection linking to navigation detail

--- resources/views/components/navigation-detail.blade.php
+++ resources/views/components/navigation-detail.blade.php
@@ -3,7 +3,9 @@
 use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
+use Noerd\Cms\Helpers\CollectionHelper;
 use Noerd\Cms\Helpers\FieldHelper;
+use Noerd\Cms\Models\Collection;
 use Noerd\Cms\Models\Navigation;
 use Noerd\Cms\Models\Page;
 use Noerd\Traits\NoerdDetail;
@@ -19,6 +21,7 @@ new class extends Component {
     public function mount(): void
     {
         $this->initDetail();
+        $this->injectCollectionOptions();
 
         $navigation = new Navigation;
         if ($this->modelId) {
@@ -29,6 +32,27 @@ new class extends Component {
 
         if ($navigation['page_id']) {
             $this->pageSelected($navigation['page_id']);
+        }
+    }
+
+    private function injectCollectionOptions(): void
+    {
+        $tenantId = auth()->user()->selected_tenant_id;
+        $collections = Collection::where('tenant_id', $tenantId)->orderBy('name')->get();
+
+        $options = [['value' => '', 'label' => '-- ' . __('noerd_please_select') . ' --']];
+        foreach ($collections as $collection) {
+            $config = CollectionHelper::getCollectionFields(strtolower($collection->collection_key));
+            if ($config && !empty($config['hasPage'])) {
+                $options[] = ['value' => $collection->id, 'label' => $collection->name];
+            }
+        }
+
+        foreach ($this->pageLayout['fields'] as &$field) {
+            if (($field['name'] ?? '') === 'detailData.collection_id') {
+                $field['options'] = $options;
+                break;
+            }
         }
     }
 
@@ -60,13 +84,16 @@ new class extends Component {
 
     public function store(): void
     {
+        $hasCollection = !empty($this->detailData['collection_id']);
+
         $this->validate([
             'detailData.navigation_key' => ['required', 'string', 'max:255'],
             'detailData.name' => ['required', 'array'],
             'detailData.parent_id' => ['nullable', 'numeric', 'exists:cms_navigations,id'],
             'detailData.sort_order' => ['nullable', 'integer', 'min:0'],
-            'detailData.page_id' => ['nullable', 'numeric', 'required_without:detailData.link'],
-            'detailData.link' => ['nullable', 'string', 'max:2048', 'required_without:detailData.page_id'],
+            'detailData.page_id' => ['nullable', 'numeric', $hasCollection ? '' : 'required_without:detailData.link'],
+            'detailData.link' => ['nullable', 'string', 'max:2048', $hasCollection ? '' : 'required_without:detailData.page_id'],
+            'detailData.collection_id' => ['nullable', 'numeric', 'exists:collections,id'],
             'detailData.new_tab' => ['nullable', 'boolean'],
         ]);
 
@@ -75,7 +102,12 @@ new class extends Component {
         // TODO auto detect if value is an array and convert it to JSON
         $data['name'] = json_encode($data['name']);
 
-        if (isset($data['link'])) {
+        $data['collection_id'] = !empty($data['collection_id']) ? (int) $data['collection_id'] : null;
+
+        if ($data['collection_id']) {
+            $data['page_id'] = null;
+            $data['link'] = null;
+        } elseif (isset($data['link'])) {
             $data['link'] = trim((string) $data['link']) ?: null;
             $data['page_id'] = null;
         }
